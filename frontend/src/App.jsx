d08c3feda6dbe94f8ab2ccc9e28dc5a47e8190b4
kevinlampe1998 <kevin.lampe@dci-student.org>
2024-07-17 11:34:43 +0200

    created template

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import AppProvider from "./context/AppProvider";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SharedLayout from "./pages/SharedLayout";
+import Home from "./pages/Home/Home";
+import NotFound from "./pages/NotFound/NotFound";
+import "./css/App.css";
+
+function App() {
+  return (
+    <>
+      <AppProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<SharedLayout />}>
+              <Route index element={<Home />} />
+              {/* Weitere Routen hier zwischen */}
+
+              {/* Weitere Routen hier zwischen */}
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AppProvider>
+    </>
+  );
+}
+
+export default App;
